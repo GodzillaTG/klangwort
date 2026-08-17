22b6c128c60e7f6454f326a2cf468a67ab7e003d
Sichen Guo <sichenguo@TonyGs-MacBook-Pro-3.local>
2026-08-17 19:20:36 +0800

    Improve Tonmeister recording scenario choices

--- studio/sw.js
+++ studio/sw.js
@@ -1,5 +1,7 @@
-const CACHE_VERSION = "2844e63686cf";
+const CACHE_VERSION = "731fffef0704";
 const BUILD_ASSETS = [
+  "/klangwort/studio/_next/static/648cf80a-4bfa-4e71-832c-c75f0cc3a228/_buildManifest.js",
+  "/klangwort/studio/_next/static/648cf80a-4bfa-4e71-832c-c75f0cc3a228/_ssgManifest.js",
   "/klangwort/studio/_next/static/_vinext_fonts/geist-8ac0455e797f/geist-001175b1.woff2",
   "/klangwort/studio/_next/static/_vinext_fonts/geist-8ac0455e797f/geist-52306abf.woff2",
   "/klangwort/studio/_next/static/_vinext_fonts/geist-8ac0455e797f/geist-875ccdd4.woff2",
@@ -11,15 +13,13 @@ const BUILD_ASSETS = [
   "/klangwort/studio/_next/static/_vinext_fonts/geist-mono-00e989178794/geist-mono-44e03052.woff2",
   "/klangwort/studio/_next/static/_vinext_fonts/geist-mono-00e989178794/geist-mono-971fb274.woff2",
   "/klangwort/studio/_next/static/_vinext_fonts/geist-mono-00e989178794/geist-mono-f6b33328.woff2",
-  "/klangwort/studio/_next/static/chunks/TonmeisterApp-CPRwk3cd.js",
+  "/klangwort/studio/_next/static/chunks/TonmeisterApp-Dl9VtJiP.js",
   "/klangwort/studio/_next/static/chunks/framework-BgSIrAUN.js",
-  "/klangwort/studio/_next/static/chunks/index-CG3kAQEv.js",
-  "/klangwort/studio/_next/static/chunks/layout-segment-context-CZf7I68k.js",
+  "/klangwort/studio/_next/static/chunks/index-i0VLaOvu.js",
+  "/klangwort/studio/_next/static/chunks/layout-segment-context-Dz5HwjYt.js",
   "/klangwort/studio/_next/static/chunks/pwa-register-BWwGaK-v.js",
   "/klangwort/studio/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
   "/klangwort/studio/_next/static/css/index.B2ir9Uyv.css",
-  "/klangwort/studio/_next/static/eb223e2e-cc70-4505-ba91-c96a04fc52a9/_buildManifest.js",
-  "/klangwort/studio/_next/static/eb223e2e-cc70-4505-ba91-c96a04fc52a9/_ssgManifest.js",
   "/klangwort/studio/ab-dpa.jpg",
   "/klangwort/studio/ab-spacing-curve-dpa.jpg",
   "/klangwort/studio/comb-filtering-dpa.jpg",
